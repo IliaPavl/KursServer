ba9b9d150c02811f5413dcb122110e3384dd3867
IliaPavl <i.pavl@list.ru>
2022-04-15 12:04:20 +0300

    new page

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express()
 const PORT = process.env.PORT || 3005
 
 app.get('/', (reg,res)=>{
-    res.end('<h1> Home</h1>'+
+    res.end('<h1> Home Page</h1>'+
     '<div> <ul><li><a href ="/about"> Apout Pagr</li></ul></div>')
 })
 
 app.get('/about', (reg,res)=>{
-    res.end('<h1> About</h1>'+
+    res.end('<h1> About Page</h1>'+
     '<div> <ul><li><a href ="/"> Home Pagr</li></ul></div>')
 })
 
